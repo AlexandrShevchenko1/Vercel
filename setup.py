e7c2fa4b1daa773332a9b6904eb24902b1f3c581
AlexandrShevchenko1 <alexandrshevchenko13@mail.ru>
2023-03-19 03:55:40 +0300

    dkvkjjdjkdj

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask',
+    version='0.1',
+    description='bot',
+    author='Alex',
+    author_email='alexandrshevchenko13@mail.ru',
+    packages=find_packages(),
+)
